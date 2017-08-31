d5b9feb33de7e49eef169bb3845aab0d389d08fb
cristian <cristiangarcia@mesoamericana.edu.gt>
2017-08-30 19:35:51 -0600

    css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from.models import Publicar
 def listar(request):
-    return render(request,'blog/listar.html',{})
+    publi=Publicar.objects.filter(fecha_publica__lte=timezone.now()).order_by('fecha_publica')
+    return render(request,'blog/listar.html',{'publi':publi})
 # Create your views here.
