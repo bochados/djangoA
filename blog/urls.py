a4ed87702242afdbbfca634aac45ff0a523075e2
cristian <cristiangarcia@mesoamericana.edu.gt>
2017-09-01 21:13:28 -0600

    urltitulo

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listar),
+    url(r'postear/(?P<pk>[0-9]+)/$', views.detalle_pub, name='postear'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from.models import Publicar
 def listar(request):
     publi=Publicar.objects.filter(fecha_publica__lte=timezone.now()).order_by('fecha_publica')
     return render(request,'blog/listar.html',{'publi':publi})
+def detalle_pub(request,pk):
+    p=get_object_or_404(Publicar,pk=pk)
+    return render(request, 'blog/detalle_pub.html',{'p':p})
 # Create your views here.
